9150823476eccf69853f4dc2800fb97e8e550994
evermore210007 <evermore210007@gmail.com>
2023-11-19 12:34:56 +0800

    2023_1_19_Countdown

--- javascript/Main.js
+++ javascript/Main.js
@@ -145,6 +145,43 @@ const Main = Vue.createApp({
             $this.SettingDate = Today.getFullYear() + "/" + (Today.getMonth() + 1) + "/" + Today.getDate();
         },
 
+        Countdown:function(){
+            const second = 1000,
+        minute = second * 60,
+        hour = minute * 60,
+        day = hour * 24;
+
+  //I'm adding this section so I don't have to keep updating this pen every year :-)
+  //remove this if you don't need it
+  let today = new Date(),
+      dd = String(today.getDate()).padStart(2, "0"),
+      mm = String(today.getMonth() + 1).padStart(2, "0"),
+      yyyy = today.getFullYear(),
+      // nextYear = yyyy + 1,
+      dayMonth = "11/29/",
+      birthday = dayMonth + yyyy;
+  
+  today = mm + "/" + dd + "/" + yyyy;
+  if (today > birthday) {
+    birthday = dayMonth + nextYear;
+  }
+  //end
+  
+  const countDown = new Date(birthday).getTime(),
+      x = setInterval(function() {    
+
+        const now = new Date().getTime(),
+              distance = countDown - now;
+
+        document.getElementById("days").innerText = Math.floor(distance / (day)),
+          document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+          document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+          document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+
+        //seconds
+      }, 0)
+        },
+
     },
 
     watch: {
@@ -155,6 +192,7 @@ const Main = Vue.createApp({
         var $this = this;
         window.onload = function () {
             $this.EntryLoading();
+            $this.Countdown();
 
             // 監聽滾動事件
             window.onmousewheel = document.onmousewheel = (e) => {
